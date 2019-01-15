ec5086e499c9a6b0e015d643dec103425146ee5a
Maxime Péloquin <mpeloquin@lvlstudio.com>
2019-01-15 15:21:10 -0500

    Init

---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class test : MonoBehaviour
+{
+	private Rewired.Player _player;
+
+	private void Start()
+	{
+		_player = Rewired.ReInput.players.GetPlayer(0);
+		UnityEngine.tvOS.Remote.allowExitToHome = false;
+	}
+
+	private void Update() {
+		if(_player.GetButtonDown("UIPlay"))
+			Debug.Log("Play");
+            
+		if(_player.GetButtonDown("UICancel"))
+			Debug.Log("Cancel");
+		
+		if(_player.GetButtonDown("UISubmit"))
+			Debug.Log("Submit");
+	}
+}
+
